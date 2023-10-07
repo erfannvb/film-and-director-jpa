8cfeca6c286d28b0ad28bb8332366b81fd9b70ef
Erfan Navab <erfannavab@gmail.com>
2023-10-07 15:39:33 +0330

    implement check validation

--- src/main/java/nvb/dev/service/impl/DirectorServiceImpl.java
+++ src/main/java/nvb/dev/service/impl/DirectorServiceImpl.java
@@ -3,13 +3,17 @@ package nvb.dev.service.impl;
 import jakarta.persistence.EntityManager;
 import nvb.dev.base.service.impl.BaseServiceImpl;
 import nvb.dev.entity.Director;
+import nvb.dev.exception.ValidationException;
 import nvb.dev.repository.DirectorRepository;
 import nvb.dev.service.DirectorService;
+import nvb.dev.util.validation.CheckValidation;
 
 public class DirectorServiceImpl extends BaseServiceImpl<Long, Director, DirectorRepository>
         implements DirectorService {
 
     protected final EntityManager entityManager;
+
+    CheckValidation checkValidation = new CheckValidation();
 
     public DirectorServiceImpl(EntityManager entityManager, DirectorRepository repository) {
         super(entityManager, repository);
@@ -18,6 +22,7 @@ public class DirectorServiceImpl extends BaseServiceImpl<Long, Director, Directo
 
     @Override
     public void register(Director director) {
+        if (!checkValidation.isValid(director)) throw new ValidationException("Invalid Data!!!");
         repository.save(director);
     }
 }
