bb13f11238f8db4baea486ba169179ce0e9b14a7
Erfan Navab <erfannavab@gmail.com>
2023-10-07 15:39:08 +0330

    add not null annotation

--- src/main/java/nvb/dev/entity/Director.java
+++ src/main/java/nvb/dev/entity/Director.java
@@ -3,6 +3,7 @@ package nvb.dev.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import nvb.dev.base.entity.BaseEntity;
 
@@ -15,9 +16,11 @@ import nvb.dev.base.entity.BaseEntity;
 @ToString
 public class Director extends BaseEntity<Long> {
 
+    @NotNull(message = "firstName must not be null")
     @Column(name = "first_name")
     private String firstName;
 
+    @NotNull(message = "lastName must not be null")
     @Column(name = "last_name")
     private String lastName;
 
